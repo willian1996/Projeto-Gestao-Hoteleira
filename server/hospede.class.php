5041a853d31e505720ba3a2c2e396a36dbf72b2c
Willian Sales <williansalesgabriel@hotmail.com>
2019-05-08 05:15:48 -0300

    Update 08/05

--- server/hospede.class.php
+++ server/hospede.class.php
@@ -1,12 +1,26 @@
 <?php
-
+/*
+* CLASSE PARA HOSPEDES
+* ESTA CLASSE CRIA, LÊ, ATUALIZA E DELETA HOSPEDES
+* @package GESTÃO HOTELEIRA
+* @author WILLIAN <williansalesgabriel@hotmail.com>
+*/
 class Hospede{
     private $pdo;
-    
+    /*
+    * MÉTODO CONSTRUTOR PARA CONECTAR COM O BANCO DE DADOS 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function __construct($pdo){
         $this->pdo = $pdo;
     }
     
+    /*
+    * MÉTODO PARA CADASTRAR HOSPEDES NO BANCO DE DADOS 
+    * @param DADOS DO HOSPEDE A SER CADASTRADO 
+    * @return ARRAY BOOLEAN E MENSAGEM DE STATUS
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function cadastrarHospede($nome_completo, $CPF, $email, $celular, $telefone){
         //aplicando segurança nas entradas 
         $nome_completo = $this->filtraEntrada($nome_completo);
@@ -33,7 +47,6 @@ class Hospede{
             $retorno['mensagem'] = "Erro! CPF já está em uso em outro registro!";
             return $retorno;
         }
-        
         try{
             $sql = "INSERT INTO hospedes (nome_completo, CPF, email, celular, telefone) VALUES (:nome_completo, :CPF, :email, :celular, :telefone)";
             $sql = $this->pdo->prepare($sql);
@@ -58,13 +71,14 @@ class Hospede{
             $retorno['deucerto'] = false;
             $retorno['mensagem'] = "Erro no servidor";
             return $retorno;
-        }
-        
-            
-            
-        
+        }  
     }
     
+    /*
+    * MÉTODO PARA LISTAR HOSPEDES CADASTRADOS COM STATUS 1(ATIVO)
+    * @return ARRAY COM TODOS OS REGISTROS 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function listarHospedes(){
         $sql = "SELECT * FROM hospedes WHERE status = 1";
         $sql = $this->pdo->query($sql);
@@ -76,6 +90,12 @@ class Hospede{
         }
     }
     
+    /*
+    * MÉTODO PARA BUSCAR INFORMAÇÕES DE UM HOSPEDE 
+    * @param ID IDENTIFICADOR DO HOSPEDE 
+    * @return ARRAY COM UM REGISTRO 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function infoHospede($id){
         $sql = "SELECT * FROM hospedes WHERE id = :id";
         $sql = $this->pdo->prepare($sql);
@@ -89,6 +109,12 @@ class Hospede{
         }
     }
     
+    /*
+    * MÉTODO PARA BUSCAR INFORMAÇÕES DE UM HÓSPEDE 
+    * @param EMAIL IDENTIFICADOR DO HOSPEDE 
+    * @return ARRAY COM UM REGISTRO 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function infoHospedePorEmail($email){
         $sql = "SELECT * FROM hospedes WHERE email = :email";
         $sql = $this->pdo->prepare($sql);
@@ -102,7 +128,12 @@ class Hospede{
         }
     }
     
-    
+    /*
+    * MÉTODO PARA UTUALIZAR INFORMAÇÕES DE UM HÓSPEDE  
+    * @param DADOS DO HÓSPEDE A SER ATUALIZADOS  
+    * @return ARRAY COM UM REGISTRO 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function editarHospede($id, $nome_completo, $CPF, $email, $celular, $telefone){
         //aplicando segurança nas entradas
         $id = $this->filtraEntrada($id);
@@ -155,10 +186,15 @@ class Hospede{
             $retorno['deucerto'] = false;
             $retorno['mensagem'] = "Erro no servidor";
             return $retorno;
-        }
-            
+        }  
     }
     
+    /*
+    * MÉTODO PARA DESATIVAR O HOSPEDE   
+    * @param ID IDENTIFICADOR DO HOSPEDE 
+    * @return ARRAY COM BOOLEAN E MENSAGEM 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function excluirHospede($id){
         $id = $this->filtraEntrada($id);
         $sql = "UPDATE hospedes SET status = '0' WHERE id = :id";
@@ -177,22 +213,12 @@ class Hospede{
         }
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    /*
+    * MÉTODO PARA VERIFICAR EXISTENCIA DE EMAIL DO HOSPEDE   
+    * @param EMAIL E ID IDENTIFICADOR DO HOSPEDE 
+    * @return BOOLEAN  
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function existeEmail($email, $id=''){
         if($id == ''){
             $email = $this->filtraEntrada($email);
@@ -215,6 +241,12 @@ class Hospede{
         }
     }
     
+    /*
+    * MÉTODO PARA VERIFICAR EXISTENCIA DE CPF DO HOSPEDE   
+    * @param CPF E ID IDENTIFICADOR DO HOSPEDE 
+    * @return BOOLEAN  
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     public function existeCPF($CPF, $id=''){
         if($id == ''){
             $CPF = $this->filtraEntrada($CPF);
@@ -237,16 +269,20 @@ class Hospede{
         }
     }
     
+    /*
+    * MÉTODO PARA PROTEJER O BANCO DE DADOS DE SQL INJECTION   
+    * @param DADO A FILTRAR  
+    * @return DADO FILTRADO 
+    * @author WILLIAN <williansalesgabriel@hotmail.com>
+    */
     private function filtraEntrada($campo){
-        $campo = trim($campo); // remove espaços no início e no final
-        $campo = strip_tags($campo); // remove tags html
-        $campo = addslashes($campo); // adiciona caractere de escape nas aspas e apostófros
+        // remove espaços no início e no final
+        $campo = trim($campo); 
+        // remove tags html
+        $campo = strip_tags($campo); 
+        // adiciona caractere de escape nas aspas e apostófros
+        $campo = addslashes($campo); 
         return $campo;
     }
-    
-    
-    
-    
-    
-    
-}
+
+}/*FIM DA CLASSE HOSPEDE*/
